518a6653829158f1c7606b75e594fe1fbb4dab08
ikim025 <iankimm@Ians-MacBook-Pro.local>
2015-01-23 00:11:23 -0800

    prints $ and takes input

--- rshell.cpp
+++ rshell.cpp
@@ -3,8 +3,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+    string cmd;
+    while(cmd != "exit"){
+        std::cout << "$ ";
+        cin >> cmd;
+    }
+    return 0;
 }
