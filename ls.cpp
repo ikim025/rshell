68e67c86881cd4e239cc6054c9f6c9279d1515be
Ian Kim <ikim025@hammer.cs.ucr.edu>
2015-02-08 21:32:24 -0800

    correctly exits?

--- ls.cpp
+++ ls.cpp
@@ -10,5 +10,13 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char **argv){
+	char cmd[512];
+	int ending = 0;
+	do{
+	printf("$ ");
+	cin >> cmd;
+	if(cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'i' && cmd[3] == 't')
+		ending = 1;
+	}while(ending == 0);
 }
