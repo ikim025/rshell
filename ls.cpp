3d48a5dc801f324ccd9180d985a9a8635747dbc3
Ian Kim <ikim025@hammer.cs.ucr.edu>
2015-02-08 22:57:43 -0800

    ls -a works

--- ls.cpp
+++ ls.cpp
@@ -22,14 +22,7 @@ void ls_a(char *loc){
 	}
 	while((mf = readdir(md)) != NULL){
 		stat(mf -> d_name, &ms);
-		struct stat tp;
-		stat(mf -> d_name, &tp);
-		if(S_ISDIR(tp.st_mode)){
-			printf("%s ", mf -> d_name);
-			std :: cout << "/";
-		}
-		else
-			printf(" %s ", mf -> d_name);
+		printf(" %s ", mf -> d_name);
 	}
 	std :: cout << "\n";
 	closedir(md);
@@ -37,16 +30,17 @@ void ls_a(char *loc){
 }
 
 int main(int argc, char **argv){
-	char cmd[] = "bin";
-	ls_a(cmd);
-	/*char cmd[512];
+	char curr[] = "bin";
+	char cmd[512];
 	int ending = 0;
 	do{
 	printf("$ ");
 	cin >> cmd;
+	if(cmd[0] == 'l' && cmd[1] == 's' && cmd[3] == '-' && cmd[4] == 'a')
+		ls_a(curr);
 	if(cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'i' && cmd[3] == 't')
 		ending = 1;
 	}while(ending == 0);
 	return 0;
-*/
+
 }
