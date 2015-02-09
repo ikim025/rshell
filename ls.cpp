5d67614c0cf6a0f24a5bc8cb44f956507469bb0c
Ian Kim <ikim025@hammer.cs.ucr.edu>
2015-02-08 21:25:31 -0800

    Simple ls for now

---
+++ ls.cpp
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <dirent.h>
+
+using namespace std;
+
+int main(){
+}
