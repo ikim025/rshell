cfbc845a78864e840fb22314dc2d4db1598fb352
Ian Kim <ikim025@hammer.cs.ucr.edu>
2015-02-23 09:47:47 -0800

    had an error with Internet last night. it just got back up today right now

--- Makefile
+++ Makefile
@@ -6,10 +6,10 @@ all: rshell.o ls.o
 
 rshell.o: rshell.cpp
 	mkdir bin || [-d bin]
-	$(CC) $(FLAGS) rshell.cpp -o bin/rshell
+	$(CC) $(FLAGS) src/rshell.cpp -o bin/rshell
 
 ls.o: ls.cpp
-	$(CC) $(FLAGS) ls.cpp -o bin/ls
+	$(CC) $(FLAGS) src/ls.cpp -o bin/ls
 
 clean:
 	rm -rf bin
--- src/rshell.cpp
+++ src/rshell.cpp
@@ -8,21 +8,145 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-using namespace std;
+//Friends advised me for a global variables.
+char A[312];
+char B[312];
+int CTR1 = 0;
+int CTR2 = 0;
+int TRCK1 = 0;
+int TRCK2 = 0;
 
+//remove the lines before the file name for io redirection
 char* dwhite(char *n){
 	char d[] = {" \n\t<>"};
-	for(i = strcspn(n,d);i == 0; i = strcspn(n,d)){
-	n++;
+	int i = strcspn(n,d);
+	while(i == 0){
+		n++;
+		i = strcspn(n,d);
 	}
 	return strtok(n,d);
 }
 
-void parse(char* buffer, char* arg[]){
+//io redirection/ check
+void iod(char* buffer){
+	char *temp = buffer;
+	while(*temp != '\0'){
+		if(*temp == '<'){
+			*temp = '\0';
+			CTR1 = 1;
+			temp ++;
+			strcpy(A, dwhite(temp));
+		}
+		//wrong
+		else if(*temp == '>'){
+			*temp = '\0';
+			CTR2 = 1;
+			temp ++;
+			strcpy(B,dwhite(temp));
+		}
+		//if(*temp == '|'){
+		
+		//}
+		//else
+	temp++;
+	}
+}
+
+//
+void com(char* b, char* arg[]){
+	int i = 0;
+	char d[] = " \t\n";
+	char* counter = strtok(b,d);
+	char* temp = NULL;
+	while(counter != temp){
+		arg[i++] = counter;
+		//counter ++;
+		counter = strtok(temp,d);
+	}
+	arg[i] = temp;
+}
+
+//opening files
+void opf(){
+	if(CTR1 == 1){
+		TRCK1 = open(A, O_RDONLY);
+		if(TRCK1 == -1)
+			perror("open");
+	}
+	if(CTR2 == 1){
+		TRCK2 = open(B, O_RDWR | O_CREAT, 0666);
+		if(TRCK2 == -1)
+			perror("open");
+	}
+}
+
+//closing files
+void clf(){
+	if(TRCK1 > 0){
+		if(close(TRCK1) == -1)
+			perror("close");
+	}
+	if(TRCK2 > 0){
+        	if(close(TRCK2) == -1)
+                	perror("close");
+        }
+}
+
+void cw(char* arg[], int pi[]){
+	if(TRCK1 > 0){
+		if(dup2(TRCK1,STDIN_FILENO) < 0)
+			perror("dup2");
+		close(TRCK1);
+	}
+	if(TRCK2 > 0){
+		if(dup2(TRCK2,STDOUT_FILENO) < 0)
+			perror("dup2");
+		close(TRCK2);
+	}
+	if(execvp(arg[0],arg) < 0)
+		perror("execution");
+	exit(1);
 }
 
 int main(){
-	
+	char b[312];
+	char * arg[100];
+
+	//set up the global
+	CTR1 = 0;
+	CTR2 = 0;
+	TRCK1 = 0;
+	TRCK2 = 0;
+	while(1){	
+		fputs("$ ", stdout);
+		fgets(b, 312, stdin);
+	        iod(b);
+		com(b,arg);
+		opf();
+      		int pi[2];
+        	if( pipe(pi) < 0)
+                	perror("pipe");
+        	
+		//fork
+		int pid = fork();
+        	switch(pid){
+                	case -1:
+                       	 perror("fork");
+                      	 break;
+                	case 0:
+                         cw(arg,pi);
+                         break;
+                	default:
+                         if(wait(0) == -1)
+                                 exit(-1);
+                                 break;
+        	}
+		CTR1 = 0;
+		CTR2 = 0;
+		TRCK1 = 0;
+		TRCK2 = 0;
+		clf();
+	}
 	return 0;
 }
 
